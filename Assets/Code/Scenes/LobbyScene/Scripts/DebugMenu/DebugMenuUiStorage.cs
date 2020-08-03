937debc4fd62a95b919fb8713808c95bf18b0f1a
AlexanderPokhilenko <pokhilenko.alex@gmail.com>
2020-08-03 16:19:11 +0300

    Улучшение меню настройки графики.

--- Assets/Code/Scenes/LobbyScene/Scripts/DebugMenu/DebugMenuUiStorage.cs
+++ Assets/Code/Scenes/LobbyScene/Scripts/DebugMenu/DebugMenuUiStorage.cs
@@ -6,7 +6,9 @@ namespace Code.Scenes.LobbyScene.Scripts.DebugMenu
     public class DebugMenuUiStorage : MonoBehaviour
     {
         public GameObject debugMenuRoot;
-        public Dropdown dropdown;
-        public Slider slider;
+        public Dropdown qualityDropdown;
+        public Slider resolutionSlider;
+        public Dropdown resolutionDropdown;
+        public Button resolutionApplyButton;
     }
 }
